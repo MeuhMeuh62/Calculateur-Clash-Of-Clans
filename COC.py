0c6e94b9cc4e50ef814c8ffe93af0873cb4192a1
MeuhMeuh62 <86471514+MeuhMeuh62@users.noreply.github.com>
2022-03-10 20:13:45 +0100

    La base
    Bêta n°1

---
+++ COC.py
@@ -0,0 +1,32 @@
+hdv = int(input("Niveau hôtel de ville"))
+
+if hdv == 1:
+    print("hdv" + str(hdv))
+elif hdv == 2:
+    print("hdv" + str(hdv))
+elif hdv == 3:
+    print("hdv" + str(hdv))
+elif hdv == 4:
+    print("hdv" + str(hdv))
+elif hdv == 5:
+    print("hdv" + str(hdv))
+elif hdv == 6:
+    print("hdv" + str(hdv))
+elif hdv == 7:
+    print("hdv" + str(hdv))
+elif hdv == 8:
+    print("hdv" + str(hdv))
+elif hdv == 9:
+    print("hdv" + str(hdv))
+elif hdv == 10:
+    print("hdv" + str(hdv))
+elif hdv == 11:
+    print("hdv" + str(hdv))
+elif hdv == 12:
+    print("hdv" + str(hdv))
+elif hdv == 13:
+    print("hdv" + str(hdv))
+elif hdv == 14:
+    print("hdv" + str(hdv))
+else:
+    print("Pas entre 0 et 15 (1,2,...,13,14)")
