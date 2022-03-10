cbeff4474dcd39eaf7f89a03782df52068d4511c
MeuhMeuh62 <86471514+MeuhMeuh62@users.noreply.github.com>
2022-03-10 21:10:37 +0100

    Bêta v1
    - Continuer de noter en commentaire les objets du exel
    - Les mettre dans les if

--- Calculateur.py
+++ Calculateur.py
@@ -1,32 +1,39 @@
 hdv = int(input("Niveau hôtel de ville"))
 
-if hdv == 1:
-    print("hdv" + str(hdv))
-elif hdv == 2:
-    print("hdv" + str(hdv))
-elif hdv == 3:
-    print("hdv" + str(hdv))
-elif hdv == 4:
-    print("hdv" + str(hdv))
-elif hdv == 5:
-    print("hdv" + str(hdv))
-elif hdv == 6:
-    print("hdv" + str(hdv))
-elif hdv == 7:
-    print("hdv" + str(hdv))
-elif hdv == 8:
-    print("hdv" + str(hdv))
-elif hdv == 9:
-    print("hdv" + str(hdv))
-elif hdv == 10:
-    print("hdv" + str(hdv))
-elif hdv == 11:
-    print("hdv" + str(hdv))
-elif hdv == 12:
-    print("hdv" + str(hdv))
-elif hdv == 13:
-    print("hdv" + str(hdv))
-elif hdv == 14:
-    print("hdv" + str(hdv))
-else:
-    print("Pas entre 0 et 15 (1,2,...,13,14)")
+# A1 = Aigle artilleur
+# A2 = Arc-X
+# A3 = Canon
+# A4 = Défense anti-aérienne
+# A5 = Catapulte erratique
+# A6 = Propulseur d'air
+# A7 = Tour à bombes
+# A8 = Tour d'archer
+# A9 = Tour de l'enfer
+# A10 = Tour de sorciers
+# A11 = Tesla
+
+# B1 = Piège à ressort
+# B2 = Bombe
+# B3 = Bombe géante
+# B4 = Bombe aérienne
+# B5 = Mine chercheuse
+# B6 = Piège squelettique
+# B7 = Piège tornade
+
+# R1 = Remparts
+
+if 0 < hdv < 15:
+    if hdv >= 1:
+        if hdv >= 2:
+            if hdv >= 3:
+                if hdv >= 4:
+                    if hdv >= 5:
+                        if hdv >= 6:
+                            if hdv >= 7:
+                                if hdv >= 8:
+                                    if hdv >= 9:
+                                        if hdv >= 10:
+                                            if hdv >= 11:
+                                                if hdv >= 12:
+                                                    if hdv >= 13:
+                                                        if hdv >= 14:
